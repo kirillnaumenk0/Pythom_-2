df1b0d71b57cfb57e76f121d4fe2210b93b8fd8a
Kirill <kirillnaumenk0@mail.ru>
2022-09-28 16:41:52 +0600

    commit 1

--- №1.py
+++ №1.py
@@ -3,3 +3,13 @@
 # - 6782 -> 23
 # - 0.67 -> 13
 # - 198.45 -> 27
+
+num = str(input('Введите число: '))
+num_tr = int(float(num) * 10 **(len(num)-2))
+summa = 0
+for i in range((len(num)**2)-1):
+    if (num_tr % 10) != 0:
+        summa += (num_tr % 10)
+    num_tr //= 10
+print(summa)
+    
